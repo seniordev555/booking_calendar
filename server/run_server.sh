6cba231f916cbecbb945b74d55c66dac4db08180
seniordev555 <jacekmdev@gmail.com>
2017-10-24 06:07:01 +0200

    stage4 config

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,1 +1,1 @@
-PORT=3000 NODE_ENV=production pm2 start /var/www/new.3rdstreetadr.com_booking/v1.1.1/server/bin/www.js --name=booking_production
+PORT=3000 NODE_ENV=production pm2 start /var/www/hollywood_stage4_booking/booking_calendar/server/bin/www.js --name=booking_stage4_production
